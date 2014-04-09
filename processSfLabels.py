731b2c3d62258eb16767b6c3ee3b86c292c4c1d1
Joshua de Lange <joshua.delange@gmail.com>
2014-04-09 19:18:52 +0200

    now replaces with vf syntax!

--- processSfLabels.py
+++ processSfLabels.py
@@ -4,6 +4,7 @@ class processSfLabelsCommand(sublime_plugin.TextCommand):
 
 	edit = {}
 	view_size = 0
+	entire_buffer_region = sublime.Region(0, 0)
 
 	opening_tag = '{_"'
 	ending_tag = '"_}'
@@ -18,24 +19,31 @@ class processSfLabelsCommand(sublime_plugin.TextCommand):
 		self.view.insert(self.edit, self.view_size, "\n" + msg)
 
 
-	def processNextLabel(self, unchecked_buffer):
+	def processNextLabel(self):
 
-		start_of_label_region = unchecked_buffer.find(self.opening_tag)
-		end_of_label_region = unchecked_buffer.find(self.ending_tag)
+		start_of_label = self.view.substr(self.entire_buffer_region).find(self.opening_tag)
+		end_of_label = self.view.substr(self.entire_buffer_region).find(self.ending_tag)
 
-		label_region = sublime.Region(start_of_label_region + len(self.opening_tag), end_of_label_region)
-		label_text = unchecked_buffer[start_of_label_region + len(self.opening_tag):end_of_label_region]
+		self.log("start of label: '" + str(start_of_label) + "'")
+		self.log("end of label: '" + str(end_of_label) + "'")
+
+		label_region = sublime.Region(start_of_label + len(self.opening_tag), end_of_label)
+		label_region_to_be_replaced = sublime.Region(start_of_label, end_of_label + len(self.ending_tag))
+
+		label_text = self.view.substr(label_region)
 		label_camel_case = re.sub(r'\W+', '', ''.join(x for x in label_text.title() if not x.isspace()))
 
 		self.log("FOUND: '" + label_text + "'")
 		self.log("Camelcase: '" + label_camel_case + "'")
 		self.log("---------------------")
 
-		unchecked_buffer = unchecked_buffer[end_of_label_region + len(self.ending_tag):]
+		self.view.replace(self.edit, label_region_to_be_replaced, self.vf_opening_tag + label_camel_case + self.vf_ending_tag)
 
-		if (unchecked_buffer.find(self.opening_tag) > 0):
+		self.log("for next time: " + str(self.view.substr(self.entire_buffer_region).find(self.opening_tag)))
 
-			self.processNextLabel(unchecked_buffer)
+		if (self.view.substr(self.entire_buffer_region).find(self.opening_tag) > 0):
+
+			self.processNextLabel()
 
 
 	def run(self, edit):
@@ -43,7 +51,8 @@ class processSfLabelsCommand(sublime_plugin.TextCommand):
 		self.edit = edit
 		self.view_size = self.view.size()
 
+		self.entire_buffer_region = sublime.Region(0, self.view_size)
+
 		self.view.replace(edit, sublime.Region(0, self.view_size), '<p>{_"Label text here!"_}</p>\n<p>{_"Second label!"_}</p>\n<div>{_"Another label here!"_}</div>\n\n')
 
-		unchecked_buffer = self.view.substr(sublime.Region(0, self.view_size))
-		self.processNextLabel(unchecked_buffer)
+		self.processNextLabel()
